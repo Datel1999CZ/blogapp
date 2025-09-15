f36f14a637840f1f9c5ab43c119220f6a7022c01
David <80539564+Datel1999CZ@users.noreply.github.com>
2025-09-15 15:08:15 +0200

    Add files via upload

---
+++ final-project-wa-Datel1999CZ-main/final-project-wa-Datel1999CZ-main/website/views.py
@@ -0,0 +1,62 @@
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_required, current_user
+from .models import Post, User
+from . import db
+
+views = Blueprint("views", __name__)
+
+@views.route("/")
+def index():
+    return redirect(url_for('views.home'))
+
+@views.route("/home")
+@login_required
+def home():
+    posts = Post.query.all()
+    return render_template("home.html", user=current_user, posts=posts)
+
+
+@views.route("/create-post", methods=['GET', 'POST'])
+@login_required
+def create_post():
+    if request.method == "POST":
+        text = request.form.get('text')
+
+        if not text:
+            flash('Post cannot be empty!', category='error')
+        else:
+            post = Post(text=text, author=current_user.id)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post created!', category="success")
+            return redirect(url_for('views.home'))
+
+    return render_template('create_post.html', user=current_user)
+
+
+@views.route("/delete-post/<id>")
+@login_required
+def delete_post(id):
+    post = Post.query.filter_by(id=id).first()
+
+    if not post:
+        flash('Post does not exist', category='error')
+    elif post.author != current_user.id:
+        flash('You do not have permission to delete this post', category='error')
+    else:
+        db.session.delete(post)
+        db.session.commit()
+        flash('Post deleted.', category='success')
+
+
+    
+    return redirect(url_for('views.home'))
+
+@views.route("/posts/<username>")
+@login_required
+def user_posts(username):
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return "User Does not exist", 404
+    posts = Post.query.filter_by(author=user.id).order_by(Post.data_created.desc()).all()
+    return render_template("user_posts.html", posts=posts, username=username, user=current_user)
